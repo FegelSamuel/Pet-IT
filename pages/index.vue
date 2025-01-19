ede8209718b5c342b0ea08df12c94f9cd487d2f2
FegelSamuel <fegelsamuel@gmail.com>
2025-01-19 09:59:21 -0600

    tailwind not working

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,4 @@
 <template lang="pug">
-div
-    NuxtRouteAnnouncer
-    NuxtWelcome
+div.bg-red-200.min-h-screen.w-screen.flex.flex.col.items-center.space-y-2.space-x-8
+    button.mt-7.bg-blue-200 Hello
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,ts,vue}"],
+  content: [
+    './pages/**/*.{html,js,ts,vue,tsx}',
+    './components/**/*.{html,js,ts,vue,tsx}',
+  ],
   theme: {
     extend: {},
   },
