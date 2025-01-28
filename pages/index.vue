b556837b4402034cb97b0d34ca65ef4d877944ab
fegelsamuel <fegelsamuel@gmail.com>
2025-01-28 16:18:36 -0600

    2inova logo changed

--- pages/index.vue
+++ pages/index.vue
@@ -12,13 +12,11 @@ Body
         div() We are familiar with all the most popular Veterinary Practice Management Software programs. We work with your software provider to help during installation, upgrades, and maintenance, as well as helping to resolve problems. 
         div() Whether you are considering upgrading to a paperless (or paper-light) office, or simply have a 'bug' that's bothering you, we can help by working with your provider so you can focus on your clients.
         h2.text-xl.p-4 Links to Popular Practice Management Websites
-        div.font-bold.grid.grid-cols-4.gap-4.flex
+        div.font-bold.grid.grid-cols-3.gap-4.flex
             NuxtLink(to="https://2inova.com")
-                img(src="https://2inova.com/wp-content/uploads/2022/08/logoNewBlack.png")
+                img(src="https://2inova.com/wp-content/uploads/2022/08/favicon.png")
             NuxtLink(to="https://covetrus.com")
                 img(src="https://upload.wikimedia.org/wikipedia/commons/4/4b/Covetrus_Logo.png")
             NuxtLink(to="https://www.idexx.com/en/")
                 img(src="https://download.logo.wine/logo/Idexx_Laboratories/Idexx_Laboratories-Logo.wine.png")
-
-
 </template>
